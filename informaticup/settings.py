50e1fab396330eb2d57911a50264d14cd94e6b3c
Lukas Pirch <l.pirch@tu-bs.de>
2018-10-31 12:59:02 +0100

    prepare django views and css

--- informaticup/settings.py
+++ informaticup/settings.py
@@ -54,7 +54,7 @@ ROOT_URLCONF = 'informaticup.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['preprocess/templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -116,5 +116,7 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
-
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, "static"),
+)
 STATIC_URL = '/static/'
