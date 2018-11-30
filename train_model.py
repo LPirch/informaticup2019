af0ac6403db5ea8beb35ec14675f4fc5f7401d2a
Lukas Pirch <l.pirch@tu-bs.de>
2018-11-30 18:40:30 +0100

    remove advanced preprocessing, just scale to [0,1]

--- train_model.py
+++ train_model.py
@@ -47,25 +47,25 @@ def lr_schedule(epoch):
 
 def preprocess_img(img):
 	# Histogram normalization in v channel
-	hsv = color.rgb2hsv(img)
-	hsv[:, :, 2] = exposure.equalize_hist(hsv[:, :, 2])
-	img = color.hsv2rgb(hsv)
+	#hsv = color.rgb2hsv(img)
+	#hsv[:, :, 2] = exposure.equalize_hist(hsv[:, :, 2])
+	#img = color.hsv2rgb(hsv)
 
 	# central square crop
-	min_side = min(img.shape[:-1])
-	centre = img.shape[0] // 2, img.shape[1] // 2
-	img = img[centre[0] - min_side // 2:centre[0] + min_side // 2,
-			  centre[1] - min_side // 2:centre[1] + min_side // 2,
-			  :]
+	#min_side = min(img.shape[:-1])
+	#centre = img.shape[0] // 2, img.shape[1] // 2
+	#img = img[centre[0] - min_side // 2:centre[0] + min_side // 2,
+	#		  centre[1] - min_side // 2:centre[1] + min_side // 2,
+	#		  :]
 
 	# rescale to standard size
-	img = transform.resize(img, (FLAGS.img_size, FLAGS.img_size))
+	#img = transform.resize(img, (FLAGS.img_size, FLAGS.img_size))
 
 	## We're using channels_last; we no longer need this
 	# roll color axis to axis 0
 	# img = np.rollaxis(img, -1)
 
-	return img
+	return img / 255
 
 
 def main():
